3b7e483f8c03d25788136cf0478c200a3c9133cc
sheamf <miles_shea@yahoo.com>
2015-01-25 16:37:12 -0700

    order import functionality.  pretty much the same as product import added earlier

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :orders do
+    collection do
+      get 'import'
+    end
+  end
+
   resources :accounts do 
     member do 
       get 'test_connection'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150110013420) do
+ActiveRecord::Schema.define(version: 20150125231250) do
 
   create_table "accounts", force: true do |t|
     t.string   "shopify_account_url"
@@ -21,5 +21,59 @@ ActiveRecord::Schema.define(version: 20150110013420) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "order_items", force: true do |t|
+    t.integer  "order_id"
+    t.integer  "variant_id"
+    t.integer  "shopify_product_id"
+    t.integer  "shopify_variant_id"
+    t.float    "unit_price"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id"
+  add_index "order_items", ["shopify_product_id"], name: "index_order_items_on_shopify_product_id"
+  add_index "order_items", ["shopify_variant_id"], name: "index_order_items_on_shopify_variant_id"
+  add_index "order_items", ["variant_id"], name: "index_order_items_on_variant_id"
+
+  create_table "orders", force: true do |t|
+    t.string   "number"
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "shopify_order_id"
+    t.datetime "order_date"
+    t.float    "total"
+    t.integer  "line_item_count"
+    t.string   "financial_status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.integer  "shopify_product_id"
+    t.datetime "last_shopify_sync"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "variants", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "shopify_variant_id"
+    t.string   "option1"
+    t.string   "option2"
+    t.string   "option3"
+    t.string   "sku"
+    t.string   "barcode"
+    t.float    "price"
+    t.datetime "last_shopify_sync"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "variants", ["product_id"], name: "index_variants_on_product_id"
 
 end
