db0c39faa285c91eda168433dc410f23e63de02b
sheamf <miles_shea@yahoo.com>
2015-01-25 21:16:26 -0700

    add contest functionality

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   get 'dashboard/index'
   root 'dashboard#index'
+  post 'create_contest', to: 'dashboard#create_contest'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/factories/contests.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :contest do
+    name "MyString"
+product_id 1
+start_date "2015-01-25 19:59:19"
+end_date "2015-01-25 19:59:19"
+max_results 1
+order_id 1
+  end
+
+end
